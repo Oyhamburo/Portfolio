51de167fe7b606edbb5cff55322dc8ba429e119c
Jeremias Aitor <oyhamburo.jeremias@gmail.com>
2023-03-30 22:41:29 -0300

    vercel.json

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,8 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://api-rest-portfolio.vercel.app/contact", {
+    // let response = await fetch("http://localhost:5000/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
